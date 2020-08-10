b90ec2ac5d4355d7754075c7603462905e42af53
matdon90 <mdonhefner@gmail.com>
2020-08-10 14:16:29 +0200

    Added responsive title for mobiles, dditional informations for policy and mail sending form.

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -16,10 +16,11 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'mdonhefner@gmail.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$to = 'kontakt@automationexperts.pl';
+$email_subject = "Internetowy formularz kontaktowy:  $name";
+$email_body = "Otrzymałeś wiadomość email z internetowego formularza kontaktowego.\n\n"."Tu znajdują się szczegóły:\n\nImię: $name\n\nEmail: $email_address\n\nTelefon: $phone\n\nWiadomość:\n$message";
+$headers = "From: noreplay@automationexperts.pl\nContent-Type:".
+"text/html;charset=utf-8\n";
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
