271be887d7442751b3bfe5d55a5ecf191dfbe9bf
matdon90 <matdon90@DESKTOP-IM1U1S8>
2020-08-10 14:16:29 +0200

    Added responsive title for mobiles, dditional informations for policy and mail sending form.

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -1,5 +1,4 @@
 $(function() {
-
   $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
     preventSubmit: true,
     submitError: function($form, event, errors) {
@@ -20,7 +19,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "contact_me.php",
+        url: "https://automationexperts.pl/assets/mail/contact_me.php",
         type: "POST",
         data: {
           name: name,
@@ -35,7 +34,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>Wiadomość została wysłana, wkrótce się skontaktujemy.</strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
@@ -46,14 +45,14 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Przepraszamy " + firstName + ", wygląda na to, że serwer pocztowy nie odpowiada. Spróbuj ponownie później!"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
         },
         complete: function() {
           setTimeout(function() {
-            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+            $this.prop("disabled", false);
           }, 1000);
         }
       });
